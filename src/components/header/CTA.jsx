0c7a94e88bfc3132e34178b345babaa40f154812
Himanshu <himanshudubey481@gmail.com>
2023-04-05 14:12:52 +0530

    updates

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import resume from '../../assets/doc/Himanshu-resume.docx'
 
 const CTA = () => {
   return (
     <div className="cta">
-      <a href="#" download className="btn">
+      <a href={resume} download className="btn">
         Download CV
       </a>
-      <a href="#contact" className="btn btn-primary">
+      <a  href="#contact" className="btn btn-primary">
         Let's Talk
       </a>
     </div>
